af02e2b9cc26d7c9980bc22146957671298b1be0
gaabscps <gaabscps@gmail.com>
2023-05-01 13:49:52 -0300

    🚧 Sidebar/feed adjusting sidebar/feed interaction and positioning

--- src/features/home/components/Sidebar/index.tsx
+++ src/features/home/components/Sidebar/index.tsx
@@ -21,27 +21,29 @@ export const HomeSidebar: React.FC = () => {
   return (
     <div>
       <div className="home-leftSidebar">
-        <div className="sidebarBody">
-          <div className="action-icon">
-            <TwitterIcon color="rgb(214, 217, 219)" width={32} height={32} />
+        <div className="home-leftSidebar" style={{ position: 'fixed' }}>
+          <div className="sidebarBody">
+            <div className="action-icon">
+              <TwitterIcon color="rgb(214, 217, 219)" width={32} height={32} />
+            </div>
+            <ul className="leftSidebar-container">
+              {sidebarItem.map((item, index) => (
+                <li className="leftSidebar-item" key={index}>
+                  <div className="leftSidebar-item-content">
+                    <div>{item.icon}</div>
+                    <span className="item-spam">{item.spam}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="sidebar-buttonContainer">
+              <Button content="Tweet" onClick={() => console.log('Tweet')} theme="Twitter" />
+            </div>
+            <div className="sidebar-buttonContainer1200" />
           </div>
-          <ul className="leftSidebar-container">
-            {sidebarItem.map((item, index) => (
-              <li className="leftSidebar-item" key={index}>
-                <div className="leftSidebar-item-content">
-                  <div>{item.icon}</div>
-                  <span className="item-spam">{item.spam}</span>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <div className="sidebar-buttonContainer">
-            <Button content="Tweet" onClick={() => console.log('Tweet')} theme="Twitter" />
+          <div onClick={() => console.log('tag')}>
+            <ProfileTag />
           </div>
-          <div className="sidebar-buttonContainer1200" />
-        </div>
-        <div onClick={() => console.log('tag')}>
-          <ProfileTag />
         </div>
       </div>
     </div>
