db0876d953658e5b65e7f9fe98012f3ac2f9a5db
gaabscps <gaabscps@gmail.com>
2023-05-07 05:41:05 -0300

    🚧 Home Header Added home header component

--- src/features/home/ui/index.tsx
+++ src/features/home/ui/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HomeSidebar } from '../components/Sidebar';
+import { HomeHeader } from '../components/Header/Header';
 
 export const HomeContent = () => {
+  const headerTitles = ['For You', 'Following'];
+
   return (
     <div className="home-body">
       <div className="home-container">
         <HomeSidebar />
         <div className="home-feed">
-          <div className="headerblur" />
+          <div className="headerblur flexblur">
+            <div className="header-title">
+              <span className="headerblur-title">Home</span>
+            </div>
+            <div className="header-list">
+              <HomeHeader titles={headerTitles} />
+            </div>
+          </div>
           <div className="home-feedContainer">
             <div className="home-feedContent">
               <div className="feedTextArea">
